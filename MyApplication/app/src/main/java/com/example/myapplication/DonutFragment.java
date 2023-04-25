f48cee09ce2b42d37e1832210732c7e94f4a8227
GmtDT <dylanturnerfly@gmail.com>
2023-04-25 00:17:45 -0400

    Implemented Recycler in Donut Fragment

--- MyApplication/app/src/main/java/com/example/myapplication/DonutFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/DonutFragment.java
@@ -3,62 +3,53 @@ package com.example.myapplication;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DonutFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class DonutFragment extends Fragment {
+    RecyclerView recycler;
+    private ArrayList<Item> items = new ArrayList<>();
+    private int [] itemImages = {R.drawable.tunadonut, R.drawable.mintdonut, R.drawable.spacedonut,
+            R.drawable.susdonut, R.drawable.tsaucedonut, R.drawable.glowdonut, R.drawable.strawbaybi,
+            R.drawable.chocolate, R.drawable.mac, R.drawable.cat, R.drawable.dog, R.drawable.banana};
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public DonutFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DonutFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DonutFragment newInstance(String param1, String param2) {
-        DonutFragment fragment = new DonutFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        // Required empty public constructor (???)
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donut, container, false);
+        View view = inflater.inflate(R.layout.fragment_donut, container, false);
+        recycler = view.findViewById(R.id.recyclerView);
+        setupMenuItems(); //add the list of items to the ArrayList
+        ItemsAdapter adapter = new ItemsAdapter(view.getContext(), items); //create the adapter
+        recycler.setAdapter(adapter); //bind the list of items to the RecyclerView
+        recycler.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        return view;
+    }
+
+    /**
+     * Helper method to set up the data (the Model of the MVC).
+     */
+    private void setupMenuItems() {
+        String [] itemNames = getResources().getStringArray(R.array.donut_flavors);
+        String [] itemPrices = getResources().getStringArray(R.array.donut_prices);
+        for (int i = 0; i < itemNames.length; i++) {
+            items.add(new Item(itemNames[i], itemImages[i], itemPrices[i]));
+        }
     }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,15 +6,27 @@ import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
+
+import StoreManager.MenuItem;
+import StoreManager.Order;
+import StoreManager.OrderHistory;
 
 public class MainActivity extends AppCompatActivity {
+    public static OrderHistory orderHistory;
+    public static int orderNumberGlobal;
+    public static Order currentOrderGlobal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        orderHistory = new OrderHistory();
+        orderNumberGlobal = 1;
+        currentOrderGlobal = new Order(orderNumberGlobal);
         setContentView(R.layout.activity_main);
         FragmentManager fragmentManager = getSupportFragmentManager();
-
 
         //Code for switching fragments when the Donut button is clicked.
         Button buttonDonut = findViewById(R.id.buttonDonut);
@@ -23,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 fragmentManager.beginTransaction()
                         .replace(R.id.fragmentContainer, DonutFragment.class, null)
-//                        .setRecordingAllowed(true)
                         .addToBackStack("name")
                         .commit();
             }
@@ -36,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 fragmentManager.beginTransaction()
                         .replace(R.id.fragmentContainer, CoffeeFragment.class, null)
-//                        .setRecordingAllowed(true)
                         .addToBackStack("name")
                         .commit();
             }
@@ -49,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 fragmentManager.beginTransaction()
                         .replace(R.id.fragmentContainer, CartFragment.class, null)
-//                        .setRecordingAllowed(true)
                         .addToBackStack("name")
                         .commit();
             }
@@ -62,7 +71,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 fragmentManager.beginTransaction()
                         .replace(R.id.fragmentContainer, HistoryFragment.class, null)
-//                        .setRecordingAllowed(true)
                         .addToBackStack("name")
                         .commit();
             }
